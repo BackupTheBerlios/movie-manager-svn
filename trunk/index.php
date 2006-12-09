5bd81312cdf95e4910f5fad3649be57f59ea77fb
msn_exploder <msn_exploder@3113cd7a-0d1e-0410-bc0b-f0fcc482d0b5>
2006-12-09 09:21:52 +0000

    added libs from adodb and smarty.

---
+++ trunk/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/*******************************************************************************
+
+ LICENSE
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License (GPL)
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ To read the license please visit http://www.gnu.org/copyleft/gpl.html
+
+*******************************************************************************/
+
+// require some things
+require_once("lib/smarty/Smarty.class.php");
+require_once("lib/adodb/adodb.inc.php");
+
+php?>
